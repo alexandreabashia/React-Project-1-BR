49fee9091b110b6172a3fc2007064587fc28d690
unknown <alexandreabashia@gmail.com>
2021-04-01 03:31:04 +0400

    delete input type DATE and use custom MONTH DAY input, its just a reminder

--- src/components/AddUserCard.js
+++ src/components/AddUserCard.js
@@ -4,29 +4,46 @@ import { Link } from "react-router-dom";
 
 function AddUserCard({ users, setUsers }) {
     const [name, setName] = useState();
-    const [imageLink, setimageLink] = useState();
-    const [birthDay, setbirthDay] = useState();
+    const [imageLink, setimageLink] = useState('https://source.unsplash.com/random');
     var month = 0;
     var day = 0;
+    var months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
 
-    function setNamef(e){
+    function setNamef(e) {
         setName(e.target.value)
 
     }
-    function setImageLinkf(e){
+    function setImageLinkf(e) {
         setimageLink(e.target.value)
     }
-    function setDatef(e){
-        month = e.target.value.replace(/-0+/g, '-').slice(5,6);
-        day = e.target.value.replace(/-0+/g, '-').slice(7,10);
+    function setMonth(e) {
+        // month = e.target.value.replace(/-0+/g, '-').slice(5,6);
+        // day = e.target.value.replace(/-0+/g, '-').slice(7,10);
+        month = e.target.value;
 
     }
+    function setDay(e) {
+        day = e.target.value;
+    }
 
-    function onSubmit(){
+    function onSubmit() {
         setUsers(users.concat({
-            id: users.length + 1, 
-            name: name, 
-            image: imageLink, 
+            id: users.length + 1,
+            name: name,
+            image: imageLink,
             month: month,
             day: day
         }))
@@ -34,12 +51,14 @@ function AddUserCard({ users, setUsers }) {
 
     }
 
+    // https://source.unsplash.com/random
     return (
         <div className='adduserCard'>
             <h3 className='adduserTitle'>Add User</h3>
             <label>Name: </label><input onChange={setNamef} type="text" id='name'></input><br />
-            <label>Image Link: </label><input onChange={setImageLinkf} type="text" id='imageLink'></input><br />
-            <label>Birthday: </label><input onChange={setDatef} type="date" id='date'></input>
+            <label>Image Link: </label><input onChange={setImageLinkf} value={imageLink} type="text"></input><br />
+            <label>Month: </label><input onChange={setMonth} type="number" min="1" max="12" id='date'></input>
+            <label>Day: </label><input onChange={setDay} type="number" min="1" max="31" id='date'></input>
             <Link to="/">
                 <button onClick={onSubmit} className='addUser'>Submit</button>
             </Link>
